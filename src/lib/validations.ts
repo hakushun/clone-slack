470f4c558b45e71e440f22ba1ceff2975183a2b7
Shun Nakano <nakano_syun@waku-2.com>
2021-01-18 18:06:15 +0900

    feat: validation用の関数作成

---
+++ src/lib/validations.ts
@@ -0,0 +1,18 @@
+export const isRequired = (name: string) => (value: string): null | string =>
+  value ? null : `${name} is Required`;
+
+// eslint-disable-next-line no-useless-escape
+const mailRegexp = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+export const isEmail = (value: string): null | string =>
+  value.match(mailRegexp) ? null : 'Is not Email';
+
+export const minValue = (min: number) => (value: string): null | string =>
+  value.length >= min ? null : `Should be greater than ${min}`;
+
+export const mxaValue = (max: number) => (value: string): null | string =>
+  value.length <= max ? null : `Should be shorter than ${max}`;
+
+export const composeValidators = (...validators: any[]) => (
+  value: string,
+): null | string =>
+  validators.reduce((error, validator) => error || validator(value), null);
