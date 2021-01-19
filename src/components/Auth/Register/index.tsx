50229a8a4490de9843d1032e26ce939290dadbe2
Shun Nakano <nakano_syun@waku-2.com>
2021-01-19 18:45:51 +0900

    feta: Registerコンポーネントの実装

--- src/components/Auth/Register/index.tsx
+++ src/components/Auth/Register/index.tsx
@@ -1,6 +1,8 @@
 import Link from 'next/link';
-import React from 'react';
+import { useRouter } from 'next/router';
+import React, { useState } from 'react';
 import { Form, Field } from 'react-final-form';
+import md5 from 'md5';
 import {
   composeValidators,
   isEmail,
@@ -8,30 +10,60 @@ import {
   minValue,
   mxaValue,
 } from '../../../lib/validations';
+import firebase from '../../../lib/firebase/firebase';
 
 export const Register: React.VFC = () => {
-  const handleOnSubmit = (values: any) => {
-    console.log(values);
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const handleOnSubmit = async (values: any) => {
+    setIsLoading(true)
+    const { username, email, password } = values;
+
+    try {
+      const createdUser = await firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password);
+
+      if (!createdUser.user) throw new Error();
+
+      await createdUser.user.updateProfile({
+        displayName: username,
+        photoURL: `http://gravatar.com/avatar/${md5(
+          createdUser.user.uid,
+        )}?d=identicon`,
+      });
+
+      await firebase.database().ref('users').child(createdUser.user.uid).set({
+        name: createdUser.user.displayName,
+        avatar: createdUser.user.photoURL,
+      });
+      router.push('/');
+      setIsLoading(false);
+    } catch (err) {
+      console.log(err);
+      setIsLoading(false);
+    }
   };
 
   return (
-    <section>
+    <section id="register" className="w-full h-screen px-4 flex flex-col justify-center items-center">
       <Form
         onSubmit={handleOnSubmit}
         initialValues={{}}
-        subscription={{ submitting: true, pristine: true }}
-        render={({ handleSubmit, submitting, pristine }) => (
-          <form onSubmit={handleSubmit}>
+        subscription={{ pristine: true }}
+        render={({ handleSubmit, pristine }) => (
+          <form onSubmit={handleSubmit} className="px-10 md:px-20 py-5 bg-white rounded shadow">
             <fieldset>
               <legend>
-                <h2>Register for Clone-Slack</h2>
+                <h2 className="py-3 text-2xl">Register for Clone-Slack</h2>
               </legend>
-              <div>
+              <div className="flex flex-col">
                 <Field
                   type="text"
                   name="username"
                   validate={composeValidators(
-                    isRequired('Username'),
+                    isRequired('User name'),
                     minValue(2),
                     mxaValue(50),
                   )}
@@ -43,23 +75,26 @@ export const Register: React.VFC = () => {
                   }}>
                   {({ input, meta }) => (
                     <>
-                      <label htmlFor="username">Username</label>
+                      <label htmlFor="username" className="text-lg">User name</label>
                       <input
                         id="username"
-                        placeholder="Username"
+                        placeholder="User name"
                         required
                         aria-required
                         minLength={2}
                         maxLength={50}
-                        disabled={submitting}
+                        disabled={isLoading}
+                        className="text-lg"
                         {...input}
                       />
-                      {meta.error && meta.touched && <span>{meta.error}</span>}
+                      <div className="h-7">
+                        {meta.error && meta.touched && <span className="text-lg text-red-500">{meta.error}</span>}
+                      </div>
                     </>
                   )}
                 </Field>
               </div>
-              <div>
+              <div className="flex flex-col">
                 <Field
                   type="text"
                   name="email"
@@ -75,21 +110,23 @@ export const Register: React.VFC = () => {
                   }}>
                   {({ input, meta }) => (
                     <>
-                      <label htmlFor="email">Email</label>
+                      <label htmlFor="email" className="text-lg">Email</label>
                       <input
                         id="email"
                         placeholder="Email Address"
                         required
                         aria-required
-                        disabled={submitting}
+                        disabled={isLoading}
+                        className="text-lg"
                         {...input}
                       />
-                      {meta.error && meta.touched && <span>{meta.error}</span>}
-                    </>
+                      <div className="h-7">
+                        {meta.error && meta.touched && <span className="text-lg text-red-500">{meta.error}</span>}
+                      </div>                    </>
                   )}
                 </Field>
               </div>
-              <div>
+              <div className="flex flex-col">
                 <Field
                   type="password"
                   name="password"
@@ -106,7 +143,7 @@ export const Register: React.VFC = () => {
                   }}>
                   {({ input, meta }) => (
                     <>
-                      <label htmlFor="password">Password</label>
+                      <label htmlFor="password" className="text-lg">Password</label>
                       <input
                         id="password"
                         placeholder="Password"
@@ -115,24 +152,26 @@ export const Register: React.VFC = () => {
                         autoComplete="new-password"
                         minLength={6}
                         maxLength={50}
-                        disabled={submitting}
+                        disabled={isLoading}
+                        className="text-lg"
                         {...input}
                       />
-                      {meta.error && meta.touched && <span>{meta.error}</span>}
-                    </>
+                      <div className="h-7">
+                        {meta.error && meta.touched && <span className="text-lg text-red-500">{meta.error}</span>}
+                      </div>                    </>
                   )}
                 </Field>
               </div>
             </fieldset>
-            <div>
-              <button type="submit" disabled={submitting || pristine}>
+            <div className="flex justify-center">
+              <button type="submit" disabled={isLoading || pristine}>
                 Register
               </button>
             </div>
           </form>
         )}
       />
-      <div>
+      <div className="mt-10 px-10 md:px-20 py-5 bg-white rounded shadow">
         <div>
           Already registered?{' '}
           <Link href="/login">
