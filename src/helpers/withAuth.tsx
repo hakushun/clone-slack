cda373897acaca9ba87340a0a6ff339c3bc778db
Shun Nakano <nakano_syun@waku-2.com>
2021-01-19 18:44:59 +0900

    feat: HOC withAuthの作成

---
+++ src/helpers/withAuth.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { useDispatch, useSelector } from 'react-redux';
+import firebase from '../lib/firebase/firebase';
+import { authUser, selectIsAuth } from '../redux/modules/user';
+import { mapUserData } from '../lib/firebase/mapUserData';
+
+export const withAuth = (Component: React.FC): React.FC => (
+  props: any,
+): JSX.Element => {
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const isAuth = useSelector(selectIsAuth);
+
+  useEffect(() => {
+    const cancelAuthListener = firebase.auth().onIdTokenChanged(async (usr) => {
+      if (usr) {
+        const userData = mapUserData(usr);
+        dispatch(authUser(userData));
+      } else {
+        dispatch(authUser(null));
+        router.push('/login');
+      }
+    });
+    return () => {
+      cancelAuthListener();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return <>{!isAuth ? <div>Loading...</div> : <Component {...props} />}</>;
+};
