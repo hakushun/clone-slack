524637179b380fd09667a5bc8d778d7d4b83ba49
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-01-23 15:06:19 +0900

    refactor: validations関数にnull check追加

--- src/lib/validations.ts
+++ src/lib/validations.ts
@@ -3,14 +3,20 @@ export const isRequired = (name: string) => (value: string): null | string =>
 
 // eslint-disable-next-line no-useless-escape
 const mailRegexp = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-export const isEmail = (value: string): null | string =>
-  value.match(mailRegexp) ? null : 'Is not Email';
+export const isEmail = (value: string): null | string => {
+  if (!value) return null;
+  return value.match(mailRegexp) ? null : 'Is not Email';
+};
 
-export const minValue = (min: number) => (value: string): null | string =>
-  value.length >= min ? null : `Should be greater than ${min}`;
+export const minValue = (min: number) => (value: string): null | string => {
+  if (!value) return null;
+  return value.length >= min ? null : `Should be greater than ${min}`;
+};
 
-export const mxaValue = (max: number) => (value: string): null | string =>
-  value.length <= max ? null : `Should be shorter than ${max}`;
+export const mxaValue = (max: number) => (value: string): null | string => {
+  if (!value) return null;
+  return value.length <= max ? null : `Should be shorter than ${max}`;
+};
 
 export const composeValidators = (...validators: any[]) => (
   value: string,
