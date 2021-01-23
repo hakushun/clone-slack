f1d4c71d4d05dd226f8308533e5c2541973ad1d5
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-01-23 15:07:42 +0900

    style: overlayにスタイル追加（子要素をセンター配置に）

--- src/components/Overlay/index.tsx
+++ src/components/Overlay/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 
 export const Overlay: React.FC = ({ children }) =>
   ReactDOM.createPortal(
-    <div className="fixed inset-0 z-10 bg-black bg-opacity-80">{children}</div>,
+    <div className="fixed inset-0 z-10 p-4 flex justify-center items-center bg-black bg-opacity-80">
+      {children}
+    </div>,
     document.getElementById('overlay')!,
   );
