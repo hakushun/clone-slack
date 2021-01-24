483ec17c41a5e739a18b2a3dba7c2161bac2e6b8
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-01-24 17:47:47 +0900

    refactor: 作成したコンポーネントの配置

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { withAuth } from '../../helpers/withAuth';
+import { ColorPanel } from '../ColorPanel';
+import { Messages } from '../Messages';
 import { SideMenu } from '../SideMenu';
 
 export const Component: React.VFC = () => (
-  <div id="app" className="grid grid-cols-5 h-screen">
-    <SideMenu />
-    <section className="bg-gray-100 col-span-3">Messages</section>
-    <aside className="bg-blue-100">Meta</aside>
+  <div id="app" className="flex">
+    <ColorPanel />
+    <div className="grid grid-cols-5 h-screen flex-auto">
+      <SideMenu />
+      <Messages />
+      <aside className="bg-blue-100">Meta</aside>
+    </div>
   </div>
 );
 
