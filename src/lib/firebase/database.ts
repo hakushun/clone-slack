56dd8c5bab9265edee5f93b72c047b9b4562336b
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-01-24 17:43:00 +0900

    chore: messageRefの追加、TIMESTAMP変数作成

--- src/lib/firebase/database.ts
+++ src/lib/firebase/database.ts
@@ -2,6 +2,9 @@ import firebase from './firebase';
 
 export const usersRef = firebase.database().ref('users');
 export const channelsRef = firebase.database().ref('channels');
+export const messagesRef = firebase.database().ref('messages');
+
+export const { TIMESTAMP } = firebase.database.ServerValue;
 
 export const createUser = async (
   userCredential: firebase.auth.UserCredential,
