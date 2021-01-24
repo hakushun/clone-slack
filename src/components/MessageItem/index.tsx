a214d761bdb790cb725b969a48b8bdcee834d573
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-01-24 17:46:22 +0900

    feat: MessageItemコンポーネントの作成

---
+++ src/components/MessageItem/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { getTimeFromNow } from '../../lib/date';
+import { Message } from '../../redux/modules/message';
+
+type Props = {
+  messages: Message[];
+};
+export const MessageItem: React.VFC<Props> = ({ messages }) => (
+  <>
+    {messages.map((message) => (
+      <li key={message.timestamp} className="flex mt-3 first:m-0">
+        <img
+          src={message.user.avatarURL}
+          alt="your avatar"
+          width="36"
+          height="36"
+          className="w-9 h-9 mt-1 mr-3 rounded"
+        />
+        <div className="flex-auto">
+          <span className="text-lg font-bold">{message.user.username}</span>
+          <span className="ml-3 text-sm text-gray-500">
+            {getTimeFromNow(message.timestamp)}
+          </span>
+          <div>{message.content}</div>
+        </div>
+      </li>
+    ))}
+  </>
+);
