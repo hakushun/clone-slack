5954dea30ce1941c971cb2d88d66ef48b6f44d34
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-01-24 17:42:10 +0900

    feat: date.tsの作成

---
+++ src/lib/date.ts
@@ -0,0 +1,7 @@
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+
+dayjs.extend(relativeTime);
+
+export const getTimeFromNow = (timestamp: number): string =>
+  dayjs(timestamp).fromNow();
