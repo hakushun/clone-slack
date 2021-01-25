a9a0deb62ad398a94be4c4de0cc09566e853af50
Shun Nakano <nakano_syun@waku-2.com>
2021-01-25 17:48:49 +0900

    feat: modules/modalにuploadMediaFormを追加

--- src/redux/modules/modal.ts
+++ src/redux/modules/modal.ts
@@ -6,24 +6,36 @@ import { RootState } from './reducers';
 const actionCreator = actionCreatorFactory();
 
 export const toggleChannelForm = actionCreator<boolean>('TOGGLE_CHANNEL_FORM');
+export const toggleUploadMediaForm = actionCreator<boolean>(
+  'TOGGLE_UPLOAD_MEDIA_FORM',
+);
 
 const INITIAL_STATE: {
   channleForm: boolean;
+  uploadMediaForm: boolean;
 } = {
   channleForm: false,
+  uploadMediaForm: false,
 };
 
-const reducer = reducerWithInitialState(INITIAL_STATE).case(
-  toggleChannelForm,
-  (state, payload) => ({
+const reducer = reducerWithInitialState(INITIAL_STATE)
+  .case(toggleChannelForm, (state, payload) => ({
     ...state,
     channleForm: payload,
-  }),
-);
+  }))
+  .case(toggleUploadMediaForm, (state, payload) => ({
+    ...state,
+    uploadMediaForm: payload,
+  }));
 
 export default reducer;
 
 export const selectChannelForm = createSelector(
   [(state: RootState) => state.ui.modal.channleForm],
   (channleForm) => channleForm,
+);
+
+export const selectUploadMediaForm = createSelector(
+  [(state: RootState) => state.ui.modal.uploadMediaForm],
+  (uploadMediaForm) => uploadMediaForm,
 );
