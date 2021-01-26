d844d6d666ab2a98ada3b21f692eb802d5ed8d18
Shun Nakano <nakano_syun@waku-2.com>
2021-01-26 13:53:26 +0900

    refactor: 切り出したコンポーネントの利用

--- src/components/SideMenu/index.tsx
+++ src/components/SideMenu/index.tsx
@@ -1,10 +1,13 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { ChannelForm } from '../ChannelForm';
-import { Channels, setChannels } from '../../redux/modules/channels';
+import {
+  Channels as TypeChannels,
+  setChannels,
+} from '../../redux/modules/channels';
 import { channelsRef } from '../../lib/firebase/database';
 import { SideMenuHeader } from '../SideMenuHeader';
-import { ChannelList } from '../ChannelList';
+import { Channels } from '../Channels';
 import { AccountInfo } from '../AccountInfo';
 
 export const SideMenu: React.VFC = () => {
@@ -12,7 +15,7 @@ export const SideMenu: React.VFC = () => {
 
   useEffect(() => {
     channelsRef.on('value', (snapshots) => {
-      const loadedChannels: Channels = [];
+      const loadedChannels: TypeChannels = [];
       snapshots.forEach((snapshot) => {
         loadedChannels.push(snapshot.val());
       });
@@ -27,7 +30,7 @@ export const SideMenu: React.VFC = () => {
     <section className="bg-pink-900 text-white">
       <SideMenuHeader />
       <AccountInfo />
-      <ChannelList />
+      <Channels />
       <ChannelForm />
     </section>
   );
