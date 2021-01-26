7317ab99918de77fa58e33d0339c42d7c1ab2c8f
Shun Nakano <nakano_syun@waku-2.com>
2021-01-26 16:24:09 +0900

    feat: handleToggleDrawerの実装

--- src/components/MessagesHeaderInfo/index.tsx
+++ src/components/MessagesHeaderInfo/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { selectChannel } from '../../redux/modules/channel';
+import { selectMetaInfo, toggleMetaInfo } from '../../redux/modules/drawer';
 import { selectJoinedUsers } from '../../redux/modules/messages';
 
 export const MessagesHeaderInfo: React.VFC = () => {
+  const dispatch = useDispatch();
   const currentChannel = useSelector(selectChannel);
   const joinedUsers = useSelector(selectJoinedUsers);
+  const metaInfoIsOpened = useSelector(selectMetaInfo);
+
+  const handleToggleDrawer = () => {
+    dispatch(toggleMetaInfo(!metaInfoIsOpened));
+  };
 
   const countJointedUsers = (): string => {
     if (joinedUsers.length === 1) return '1 User';
@@ -14,7 +21,9 @@ export const MessagesHeaderInfo: React.VFC = () => {
   return (
     <div className="flex-auto">
       <h2 className="text-2xl">
-        <strong># {currentChannel.name}</strong>
+        <button type="button" onClick={handleToggleDrawer}>
+          <strong># {currentChannel.name}</strong>
+        </button>
       </h2>
       <span className="text-sm text-gray-600">{countJointedUsers()}</span>
     </div>
