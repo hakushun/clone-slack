ae33f4b4b226c94982c5d180b06bb557cceef91b
Shun Nakano <nakano_syun@waku-2.com>
2021-01-26 12:50:34 +0900

    refactor: SideMenuHeaderコンポーネントの切り出し

---
+++ src/components/SideMenuHeader/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const SideMenuHeader: React.VFC = () => (
+  <div className="flex items-center justify-center px-3 py-5 border-b border-white">
+    <img src="/images/code.svg" alt="" width="36" className="mr-2 w-9" />
+    <h1 className="text-3xl font-bold">もくもく</h1>
+  </div>
+);
