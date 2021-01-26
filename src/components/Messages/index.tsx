9a0285e063c8cb0ea56fbad1d2ce9641f3727f3e
Shun Nakano <nakano_syun@waku-2.com>
2021-01-26 16:24:39 +0900

    feat: metaInfoIsOpenedでスタイル出し分け処理追加

--- src/components/Messages/index.tsx
+++ src/components/Messages/index.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import clsx from 'clsx';
+import { useSelector } from 'react-redux';
+import { selectMetaInfo } from '../../redux/modules/drawer';
 import { MessageForm } from '../MessageForm';
 import { MessageList } from '../MessageList';
 import { MessagesHeader } from '../MessagesHeader';
 
-export const Messages: React.VFC = () => (
-  <section className="bg-gray-200 col-span-3 flex flex-col h-screen p-3 gap-4">
-    <MessagesHeader />
-    <MessageList />
-    <MessageForm />
-  </section>
-);
+export const Messages: React.VFC = () => {
+  const metaInfoIsOpened = useSelector(selectMetaInfo);
+
+  return (
+    <section
+      className={clsx(
+        'bg-gray-200 flex flex-col h-screen p-3 gap-4',
+        metaInfoIsOpened ? 'col-span-3' : 'col-span-4',
+      )}>
+      <MessagesHeader />
+      <MessageList />
+      <MessageForm />
+    </section>
+  );
+};
