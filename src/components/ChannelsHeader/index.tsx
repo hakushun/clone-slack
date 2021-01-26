a6a6944f38bdc9b668c64da2de90d9578e826d8b
Shun Nakano <nakano_syun@waku-2.com>
2021-01-26 13:53:00 +0900

    refactor: ChannelsHeaderコンポーネントの切り出し

---
+++ src/components/ChannelsHeader/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectChannels } from '../../redux/modules/channels';
+import { toggleChannelForm } from '../../redux/modules/modal';
+
+export const ChannelsHeader: React.VFC = () => {
+  const dispatch = useDispatch();
+  const channels = useSelector(selectChannels);
+
+  const openChannelForm = () => {
+    dispatch(toggleChannelForm(true));
+  };
+  return (
+    <div className="flex items-center justify-between p-3">
+      <div className="flex">
+        <img src="/images/switch.svg" alt="" width="24" />
+        <span>CHANNELS ({channels.length})</span>
+      </div>
+      <button type="button" onClick={openChannelForm}>
+        <img
+          src="/images/plus.svg"
+          alt=""
+          width="24"
+          className="w-6 rounded-full hover:bg-pink-600"
+        />
+      </button>
+    </div>
+  );
+};
