d51f65e1c4a1f8001e6e02292199882667d50a07
Shun Nakano <nakano_syun@waku-2.com>
2021-01-27 15:26:29 +0900

    feat: custom hooks useUserの作成

---
+++ src/hooks/useUser.ts
@@ -0,0 +1,65 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { connectedRef, presenceRef, usersRef } from '../lib/firebase/database';
+import { selectUser, User } from '../redux/modules/user';
+import firebase from '../lib/firebase/firebase';
+import { selectUsers, setUsers, UserInfo } from '../redux/modules/users';
+import { setPresences } from '../redux/modules/presences';
+
+type UseUserType = () => {
+  currentUser: User;
+  users: UserInfo[];
+  createUser: (_userCredential: firebase.auth.UserCredential) => Promise<void>;
+};
+
+export const useUser: UseUserType = () => {
+  const dispatch = useDispatch();
+  const currentUser = useSelector(selectUser);
+  const users = useSelector(selectUsers);
+
+  useEffect(() => {
+    if (currentUser.isAuth) {
+      usersRef.on('value', (snapshots) => {
+        const loadedUsers: UserInfo[] = [];
+        snapshots.forEach((snapshot) => {
+          loadedUsers.push(snapshot.val());
+        });
+        dispatch(setUsers(loadedUsers));
+      });
+      connectedRef.on('value', (snapshot) => {
+        if (snapshot.val() === true) {
+          const ref = presenceRef.child(currentUser.id);
+          ref.set(true);
+          ref.onDisconnect().remove();
+        }
+      });
+      presenceRef.on('value', (snapshots) => {
+        dispatch(setPresences(snapshots.val()));
+      });
+    }
+
+    return () => {
+      usersRef.off();
+      connectedRef.off();
+      presenceRef.off();
+    };
+  }, [dispatch, currentUser]);
+
+  const createUser = async (
+    userCredential: firebase.auth.UserCredential,
+  ): Promise<void> => {
+    if (!userCredential.user) throw new Error('failure to create user');
+
+    try {
+      await usersRef.child(userCredential.user.uid).set({
+        id: userCredential.user.uid,
+        username: userCredential.user.displayName,
+        avatarURL: userCredential.user.photoURL,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return { currentUser, users, createUser };
+};
