2b9857ea928df223fe90784143d1714c3a2c013e
Shun Nakano <nakano_syun@waku-2.com>
2021-01-28 13:41:18 +0900

    feat: メソッド移管とisPrivateによる出し分け追加

--- src/hooks/useMedia.ts
+++ src/hooks/useMedia.ts
@@ -1,14 +1,17 @@
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import mime from 'mime-types';
+import { v4 as uuidv4 } from 'uuid';
 import { messagesRef } from '../lib/firebase/database';
-import { uploadMediaToStorage, getDownloadURL } from '../lib/firebase/storage';
-import { selectChannel } from '../redux/modules/channel';
+import { selectChannel, selectIsPrivate } from '../redux/modules/channel';
 import {
   selectUploadMediaForm,
   toggleUploadMediaForm,
 } from '../redux/modules/modal';
 import { selectUser } from '../redux/modules/user';
 import { useMessage } from './useMessage';
+import { storageRef } from '../lib/firebase/storage';
+import firebase from '../lib/firebase/firebase';
 
 type UseMediaType = () => {
   media: File | null;
@@ -25,6 +28,7 @@ type UseMediaType = () => {
 export const useMedia: UseMediaType = () => {
   const dispatch = useDispatch();
   const currentChannel = useSelector(selectChannel);
+  const isPrivate = useSelector(selectIsPrivate);
   const currentUser = useSelector(selectUser);
   const isOpened = useSelector(selectUploadMediaForm);
   const [media, setMedia] = useState<File | null>(null);
@@ -34,6 +38,41 @@ export const useMedia: UseMediaType = () => {
   const handleChangeMedia = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files && e.target.files[0];
     setMedia(file);
+  };
+
+  const getContentType = (filename: string): string =>
+    mime.lookup(filename) || '';
+
+  const getMetadata = (filename: string): firebase.storage.UploadMetadata => ({
+    contentType: getContentType(filename),
+  });
+
+  const getExtention = (filename: string): string => {
+    const splittedFilename = filename.split('.');
+    return splittedFilename[splittedFilename.length - 1];
+  };
+
+  const getUploadFilePath = (filename: string): string =>
+    isPrivate
+      ? `chat/private/${currentChannel.id}/${uuidv4()}.${getExtention(
+          filename,
+        )}`
+      : `chat/public/${uuidv4()}.${getExtention(filename)}`;
+
+  const uploadMediaToStorage = async (
+    file: File,
+  ): Promise<firebase.storage.UploadTaskSnapshot> => {
+    const filePath = getUploadFilePath(file.name);
+    const metadata = getMetadata(file.name);
+    const result = await storageRef.child(filePath).put(file, metadata);
+    return result;
+  };
+
+  const getDownloadURL = async (
+    uploadTask: firebase.storage.UploadTaskSnapshot,
+  ): Promise<string> => {
+    const result = (await uploadTask.ref.getDownloadURL()) as string;
+    return result;
   };
 
   const createMessagewithMedia = async (
--- src/lib/firebase/storage.ts
+++ src/lib/firebase/storage.ts
@@ -1,38 +1,3 @@
-import mime from 'mime-types';
-import { v4 as uuidv4 } from 'uuid';
 import firebase from './firebase';
 
 export const storageRef = firebase.storage().ref();
-
-const getContentType = (filename: string): string =>
-  mime.lookup(filename) || '';
-
-export const getMetadata = (
-  filename: string,
-): firebase.storage.UploadMetadata => ({
-  contentType: getContentType(filename),
-});
-
-const getExtention = (filename: string): string => {
-  const splittedFilename = filename.split('.');
-  return splittedFilename[splittedFilename.length - 1];
-};
-
-export const getUploadFilePath = (filename: string): string =>
-  `chat/public/${uuidv4()}.${getExtention(filename)}`;
-
-export const uploadMediaToStorage = async (
-  media: File,
-): Promise<firebase.storage.UploadTaskSnapshot> => {
-  const filePath = getUploadFilePath(media.name);
-  const metadata = getMetadata(media.name);
-  const result = await storageRef.child(filePath).put(media, metadata);
-  return result;
-};
-
-export const getDownloadURL = async (
-  uploadTask: firebase.storage.UploadTaskSnapshot,
-): Promise<string> => {
-  const result = (await uploadTask.ref.getDownloadURL()) as string;
-  return result;
-};
