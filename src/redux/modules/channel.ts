88da6493fc3179e17c248806b8e49d2b65aad9ed
Shun Nakano <nakano_syun@waku-2.com>
2021-01-28 13:39:53 +0900

    feat: modules/channelにisPrivateプロパティ追加

--- src/redux/modules/channel.ts
+++ src/redux/modules/channel.ts
@@ -6,16 +6,22 @@ import { RootState } from './reducers';
 export type Channel = {
   id: string;
   name: string;
-  description: string;
-  createdBy: {
+  description?: string;
+  createdBy?: {
     username: string;
     avatarURL: string;
   };
+  isPrivate?: boolean;
 };
 
 const actionCreator = actionCreatorFactory();
 
-export const focusChannel = actionCreator<Channel>('FOCUS_CHANNEL');
+export const focusPublicChannel = actionCreator<Channel>(
+  'FOCUS_PUBLIC_CHANNEL',
+);
+export const focusPrivateChannel = actionCreator<Channel>(
+  'FOCUS_PRIVATE_CHANNEL',
+);
 
 const INITIAL_STATE: Channel = {
   id: '',
@@ -25,19 +31,27 @@ const INITIAL_STATE: Channel = {
     username: '',
     avatarURL: '',
   },
+  isPrivate: false,
 };
 
-const reducer = reducerWithInitialState(INITIAL_STATE).case(
-  focusChannel,
-  (state, payload) => ({
-    ...state,
+const reducer = reducerWithInitialState(INITIAL_STATE)
+  .case(focusPublicChannel, (_state, payload) => ({
     ...payload,
-  }),
-);
+    isPrivate: false,
+  }))
+  .case(focusPrivateChannel, (_state, payload) => ({
+    ...payload,
+    isPrivate: true,
+  }));
 
 export default reducer;
 
 export const selectChannel = createSelector(
   [(state: RootState) => state.ui.channel],
   (channel) => channel,
+);
+
+export const selectIsPrivate = createSelector(
+  [(state: RootState) => state.ui.channel.isPrivate],
+  (isPrivate) => isPrivate,
 );
