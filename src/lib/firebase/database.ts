23b7d378c3029d9e043d20cdb237fdca8bf1ba47
Shun Nakano <nakano_syun@waku-2.com>
2021-01-28 13:39:23 +0900

    feat: privateMessagesRefの追加

--- src/lib/firebase/database.ts
+++ src/lib/firebase/database.ts
@@ -3,6 +3,7 @@ import firebase from './firebase';
 export const usersRef = firebase.database().ref('users');
 export const channelsRef = firebase.database().ref('channels');
 export const messagesRef = firebase.database().ref('messages');
+export const privateMessagesRef = firebase.database().ref('privateMessages');
 export const presenceRef = firebase.database().ref('presence');
 
 export const connectedRef = firebase.database().ref('.info/connected');
