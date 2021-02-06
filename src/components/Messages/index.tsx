b9f02a60880d54f47075b99328710bdefae32bb1
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-02-06 15:41:39 +0900

    refactor: channelが選択されてない時に表示するDOMの作成

--- src/components/Messages/index.tsx
+++ src/components/Messages/index.tsx
@@ -5,8 +5,10 @@ import { selectMetaInfo } from '../../redux/modules/drawer';
 import { MessageForm } from '../MessageForm';
 import { MessageList } from '../MessageList';
 import { MessagesHeader } from '../MessagesHeader';
+import { useChannel } from '../../hooks/useChannel';
 
 export const Messages: React.VFC = () => {
+  const { currentChannel } = useChannel();
   const metaInfoIsOpened = useSelector(selectMetaInfo);
 
   return (
@@ -15,9 +17,17 @@ export const Messages: React.VFC = () => {
         'bg-gray-200 flex flex-col h-screen p-3 gap-4',
         metaInfoIsOpened ? 'col-span-3' : 'col-span-4',
       )}>
-      <MessagesHeader />
-      <MessageList />
-      <MessageForm />
+      {!currentChannel.id ? (
+        <div className="flex items-center justify-center flex-auto bg-gray-50 rounded shadow-md border border-gray-200 px-3 py-5">
+          <div className="text-4xl font-bold">Select a Channel</div>
+        </div>
+      ) : (
+        <>
+          <MessagesHeader />
+          <MessageList />
+          <MessageForm />
+        </>
+      )}
     </section>
   );
 };
