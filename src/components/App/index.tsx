ca61fe40a5ff61236491b07e78b9858d0e6ca0bd
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-02-11 18:20:17 +0900

    feat: ColorPanelコンポーネントの削除

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import { withAuth } from '../../helpers/withAuth';
-import { ColorPanel } from '../ColorPanel';
 import { Messages } from '../Messages';
 import { MetaInfo } from '../MetaInfo';
 import { SideMenu } from '../SideMenu';
 
 export const Component: React.VFC = () => (
-  <div id="app" className="flex">
-    <ColorPanel />
-    <div className="grid grid-cols-5 h-screen flex-auto">
-      <SideMenu />
-      <Messages />
-      <MetaInfo />
-    </div>
+  <div id="app" className="grid grid-cols-5 h-screen flex-auto">
+    <SideMenu />
+    <Messages />
+    <MetaInfo />
   </div>
 );
 
