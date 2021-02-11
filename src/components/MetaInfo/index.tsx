fc8e940fa81ad0ec65215e1a5737d6965ff42856
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-02-11 14:54:41 +0900

    refactor: MetaInfoに作成したコンポーネントを実装

--- src/components/MetaInfo/index.tsx
+++ src/components/MetaInfo/index.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { selectMetaInfo } from '../../redux/modules/drawer';
+import { useChannel } from '../../hooks/useChannel';
+import { useToggleUI } from '../../hooks/useToggleUI';
+import { MetaInfoAbout } from '../MetaInfoAbout';
+import { MetaInfoHeader } from '../MetaInfoHeader';
+import { MetaInfoMembers } from '../MetaInfoMembers';
 
 export const MetaInfo: React.VFC = () => {
-  const metaInfoIsOpened = useSelector(selectMetaInfo);
+  const { metaInfoIsOpened, handleToggleDrawer } = useToggleUI();
+  const { isPrivate } = useChannel();
 
-  return <>{metaInfoIsOpened && <aside className="bg-blue-100">Meta</aside>}</>;
+  return (
+    <>
+      {metaInfoIsOpened && (
+        <aside className="relative overflow-y-auto">
+          <MetaInfoHeader />
+          {!isPrivate && <MetaInfoAbout />}
+          <MetaInfoMembers />
+          <button
+            type="button"
+            className="absolute top-6 right-3"
+            onClick={handleToggleDrawer}>
+            <img src="/images/x.svg" alt="close metainfo" width="28" />
+          </button>
+        </aside>
+      )}
+    </>
+  );
 };
