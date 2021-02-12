6ceb544fddac2976dbbdcf5b1ed19d98af927b0b
Shun Nakano <nakano_syun@waku-2.com>
2021-02-12 17:32:13 +0900

    refactor: ChangeAvatarModalのDOM位置変更

--- src/components/AccountInfo/index.tsx
+++ src/components/AccountInfo/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import { useAvatar } from '../../hooks/useAvatar';
 import { useToggleUI } from '../../hooks/useToggleUI';
 import { AccountHeader } from '../AccountHeader';
 import { AccountMenu } from '../AccountMenu';
+import { ChangeAvatarModal } from '../ChangeAvatarModal';
 
 export const AccountInfo: React.VFC = () => {
   const { accountMenuIsOpened } = useToggleUI();
+  const { isOpened } = useAvatar();
 
   return (
     <div className="relative">
-      <AccountHeader toggleMenu={toggleMenu} />
-      {isOpend && <AccountMenu />}
+      <AccountHeader />
+      {accountMenuIsOpened && <AccountMenu />}
+      {isOpened && <ChangeAvatarModal />}
     </div>
   );
 };
--- src/components/AccountMenu/index.tsx
+++ src/components/AccountMenu/index.tsx
@@ -2,31 +2,31 @@ import React from 'react';
 import { useAuth } from '../../hooks/useAuth';
 import { useAvatar } from '../../hooks/useAvatar';
 import { useUser } from '../../hooks/useUser';
-import { ChangeAvatarModal } from '../ChangeAvatarModal';
 
 export const AccountMenu: React.VFC = () => {
   const { signOut } = useAuth();
   const { currentUser } = useUser();
-  const { isOpened, openChangeAvatarForm } = useAvatar();
+  const { openChangeAvatarForm } = useAvatar();
 
   return (
-    <div className="absolute flex flex-col justify-center w-full py-2 px-3 rounded-xl bg-white text-gray-900">
-      <span className="py-2 text-center">
-        Signed in as <strong>{currentUser.username}</strong>
-      </span>
-      <button
-        type="button"
-        onClick={openChangeAvatarForm}
-        className="py-2 rounded hover:bg-gray-300 focus:bg-gray-300">
-        Change Avatar
-      </button>
-      <button
-        type="button"
-        className="py-2 rounded hover:bg-gray-300 focus:bg-gray-300"
-        onClick={signOut}>
-        Sign Out
-      </button>
-      {isOpened && <ChangeAvatarModal />}
-    </div>
+    <>
+      <div className="absolute flex flex-col justify-center w-full py-2 px-3 rounded-xl bg-white text-gray-900">
+        <span className="py-2 text-center">
+          Signed in as <strong>{currentUser.username}</strong>
+        </span>
+        <button
+          type="button"
+          onClick={openChangeAvatarForm}
+          className="py-2 rounded hover:bg-gray-300 focus:bg-gray-300">
+          Change Avatar
+        </button>
+        <button
+          type="button"
+          className="py-2 rounded hover:bg-gray-300 focus:bg-gray-300"
+          onClick={signOut}>
+          Sign Out
+        </button>
+      </div>
+    </>
   );
 };
